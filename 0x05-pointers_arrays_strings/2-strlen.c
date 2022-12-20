b90ee402d9e00c38b9bba7d739ce230e3a72a053
Sygocha <Sadiqabdi100@gmail.com>
2022-12-20 10:56:40 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string 
+ *
+ * @s: this is the input string
+ *
+ * Return: Length of the string
+ */
+int _strlen (char *s)
+{ 
+       int index;
+       for (index = O: s[index] != '\O' ; index++)
+	return (index);
+}       
